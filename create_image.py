7e890ca4b2c029fe7e2525d49cc9b1290510930f
newbie1223 <taiti7919@gmail.com>
2024-02-26 02:53:17 +0900

    make create_image.py

---
+++ create_image.py
@@ -0,0 +1,36 @@
+# ライブラリーのインポート
+from diffusers import StableDiffusionPipeline
+import matplotlib.pyplot as plt
+
+# アクセストークンの設定
+access_tokens="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" # @param {type:"string"}
+ 
+# モデルのインスタンス化
+model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=access_tokens)
+model.to("cuda")
+
+prompt = "Tokyo Sky Tree by Marc Chagall" #@param {type:"string"}
+
+#　画像出力のディレクトリ
+import os
+outputfile = './outputfile'
+os.mkdir(outputfile)
+
+# 画像のファイル名
+import re
+filename = re.sub(r'[\\/:*?"<>|,]+', '', prompt).replace(' ','_')
+
+# 画像数
+num = 4
+ 
+for i in range(num):
+  # モデルにpromptを入力し画像生成
+  image = model(prompt,num_inference_steps=100)["sample"][0]
+  # 保存
+  outputfile = f'{filename} _{i:02} .png'
+  image.save(f"outputfile/{outputfile}")
+ 
+for i in range(num):
+  outputfile = f'{filename} _{i:02} .png'
+  plt.imshow(plt.imread(f"outputfile/{outputfile}"))
+  plt.axis('off')
