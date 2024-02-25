f8f7d5f181d6691f02f528a581886da9c38e9dbb
newbie1223 <taiti7919@gmail.com>
2024-02-26 04:15:47 +0900

    remake

--- create_image.py
+++ create_image.py
@@ -11,26 +11,11 @@ model.to("cuda")
 
 prompt = "Tokyo Sky Tree by Marc Chagall" #@param {type:"string"}
 
-#　画像出力のディレクトリ
-import os
-outputfile = './outputfile'
-os.mkdir(outputfile)
-
-# 画像のファイル名
-import re
-filename = re.sub(r'[\\/:*?"<>|,]+', '', prompt).replace(' ','_')
-
-# 画像数
-num = 1
+# モデルにpromptを入力し画像生成
+image = model(prompt,num_inference_steps=50).images[0]
+# 保存
+image.save("./sample.png")
  
-for i in range(num):
-  # モデルにpromptを入力し画像生成
-  image = model(prompt,num_inference_steps=100)["sample"][0]
-  # 保存
-  outputfile = f'{filename} _{i:02} .png'
-  image.save(f"outputfile/{outputfile}")
- 
-for i in range(num):
-  outputfile = f'{filename} _{i:02} .png'
-  plt.imshow(plt.imread(f"outputfile/{outputfile}"))
-  plt.axis('off')
+# outputfile = f'{filename} _{i:02} .png'
+# plt.imshow(plt.imread(f"outputfile/{outputfile}"))
+# plt.axis('off')
